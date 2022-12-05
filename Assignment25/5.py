d9d9b1520ff6eff363fd1226f60ab04570ed38d0
Hoda Shad <99975022+Hoda-Shad@users.noreply.github.com>
2022-12-06 01:12:05 +0330

    Add files via upload

---
+++ Assignment25/5.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+def filtering(k,img):
+
+    rows, cols = frame.shape
+    result = np.zeros(frame.shape, int)
+    mask1 = np.ones((k, k), int) / k*k
+    # mask1 = (1/k*k) * (np.ones((k,k), dtype = int))
+    for i in range(k//2 , rows - k//2):
+        for j in range(k//2 , cols - k//2):
+            # print(i,j,k)
+            small_img = img[i - k // 2 : i + k // 2 + 1 , j - k // 2 : j + k // 2 + 1]
+            result[i, j] = np.sum(small_img * mask1)
+    return (result)
+
+
+
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if ret == False:
+        break
+
+    filter_frame = filtering(3,frame)
+    filter_frame = filter_frame.astype(np.uint8)
+    print(filter_frame)
+    target = np.average(filter_frame[180:300, 250:350])
+
+    if target <= 85:
+        cv2.putText(filter_frame, "Black", (10, 60), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
+    elif target > 85 and target <= 150:
+        cv2.putText(filter_frame, "Gray", (10, 60), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
+    elif target > 150:
+        cv2.putText(filter_frame, "White", (10, 60), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
+
+    cv2.rectangle(filter_frame, (250, 180), (350, 300), (0, 255, 0), 2)
+    cv2.imshow('output', filter_frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    cv2.waitKey(10)
