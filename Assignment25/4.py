73a8c83c0e0971b839b29b895259c2729c77a65b
Hoda Shad <99975022+Hoda-Shad@users.noreply.github.com>
2022-12-06 01:12:33 +0330

    Add files via upload

---
+++ Assignment25/4.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("girl.png", 0)
+result = np.zeros(img.shape)
+rows, cols = img.shape
+filter_size = int(input("Enter size of filter: "))
+
+
+def filtering(k,img):
+    mask1 = np.ones((k,k)) / (k * k)
+    # print(mask1)
+    for i in range(k//2 , rows - k//2):
+        for j in range(k//2 , cols - k//2):
+            print(i,j,k)
+            small_img = img[i - k // 2 : i + k // 2 + 1 , j - k // 2 : j + k // 2 + 1]
+            result[i, j] = np.sum(small_img * mask1)
+    return (result)
+
+
+result = filtering(filter_size, img)
+cv2.imwrite("res.jpg", result)
+
+
+
+
