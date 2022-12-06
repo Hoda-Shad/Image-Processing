91bfd68ec1738ac92875ff30dc60937e995a01b3
Hoda Shad <99975022+Hoda-Shad@users.noreply.github.com>
2022-12-06 11:50:21 +0330

    Update 3_2.py

--- Assignment25/3_2.py
+++ Assignment25/3_2.py
@@ -2,7 +2,7 @@ import cv2
 import numpy
 import numpy as np
 
-img = cv2.imread("building.tif",0)
+img = cv2.imread("image/building.tif",0)
 rows , cols = img.shape
 result = np.zeros(img.shape)
 mask = np.array([[-1,-1,-1],[-0,0,0],[1,1,1]])
@@ -12,4 +12,4 @@ for i in range (1,rows-1):
         small_img = img[i-1:i+2, j-1:j+2]
         result[i,j] = np.sum(mask*small_img)
 
-cv2.imwrite('result3_2.jpg',result)
+cv2.imwrite('image/result3_2.jpg',result)
