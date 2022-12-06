7494f4d14d07cc2f16e8398a2dd64f5f250fd025
Hoda Shad <99975022+Hoda-Shad@users.noreply.github.com>
2022-12-06 11:49:14 +0330

    Update 2.py

--- Assignment25/2.py
+++ Assignment25/2.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img = cv2.imread("lion.png", 0)
+img = cv2.imread("image/lion.png", 0)
 rows , cols = img.shape
 result = np.zeros(img.shape)
 mask = np.array([[0,-1,0], [-1,4,-1], [0,-1,0]]) / 9
@@ -10,4 +10,4 @@ for i in range(1,rows-1):
         small_img = img[i-1: i+2 , j-1:j+2]
         result[i][j] = np.sum(mask * small_img)
 
-cv2.imwrite('result1.jpg',result)
+cv2.imwrite('image/result1.jpg',result)
